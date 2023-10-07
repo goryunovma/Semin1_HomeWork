4609fe199ba6df91d5cfc338607352139c59877e
Maxim <maximgor1980@list.ru>
2023-10-07 22:10:24 +0300

    Zadanie seminara

--- Program.cs
+++ Program.cs
@@ -26,3 +26,54 @@
 //     current ++;
 // }
 
+// Console.Write("Input a=  ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input b=  ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// if(a== b * b)
+// {
+//     Console.WriteLine($"{a} {b} True");
+// }
+// else
+// {
+//     Console.WriteLine($"{a} {b} Error");
+// }
+
+// Console.Write("Input a=  ");
+// // int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input a=  ");
+int n = Convert.ToInt32(Console.ReadLine());
+if(n == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+else if(n == 2)
+{
+    Console.WriteLine("Вторник");
+}
+else if(n == 3)
+{
+    Console.WriteLine("Среда");
+}
+else if(n == 4)
+{
+    Console.WriteLine("Четверг");
+}
+else if(n == 5)
+{
+    Console.WriteLine("Пятница");
+}
+else if(n == 6)
+{
+    Console.WriteLine("Суббота");
+}
+else if(n == 7)
+{
+    Console.WriteLine("Воскресенье");
+}
+else
+{
+    Console.WriteLine("Error");
+}
+
