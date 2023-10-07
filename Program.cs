7584c8246bce00f09fd991b5197fd2d1e73adec2
Maxim <maximgor1980@list.ru>
2023-10-07 21:43:03 +0300

    Zadanie seminara

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿// System.Console.Write("Input Number=  ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine($"You input -> {num}");
+
+// Console.Write("Input Number=  ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine($"{num} -> {num * num}");
+
+// Console.Write("Input Number=  ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// if(num >= 100 && num <=999)
+// {
+//     Console.WriteLine($"{num} -> {num % 10}");
+// }
+// else
+// {
+//     Console.Write("You input not three-digit number");
+// }
+
+// Console.Write("Input N=  ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int current = -n;
+// while(n >= current)
+// {
+//     Console.Write(current + " ");
+//     current ++;
+// }
+
